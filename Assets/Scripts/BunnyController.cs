0d26b92144f23dacdc0caf6b183907b31f00f929
Xu-Derrick <47952733+Xu-Derrick@users.noreply.github.com>
2019-08-04 21:14:37 +1200

    Added bunny animations.

--- Assets/Scripts/BunnyController.cs
+++ Assets/Scripts/BunnyController.cs
@@ -5,21 +5,26 @@ using UnityEngine;
 public class BunnyController : MonoBehaviour{
 
     private Rigidbody2D myRigidBody;
+    private Animator myAnim;
     public float bunnyJumpForce = 500f;
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetButtonUp("Jump"))
+
             {
-            myRigidBody.AddForce(transform.up * bunnyJumpForce);
+                myRigidBody.AddForce(transform.up * bunnyJumpForce);
+
             }
 
+        myAnim.SetFloat("vVelocity", myRigidBody.velocity.y);
     }
     
 }
